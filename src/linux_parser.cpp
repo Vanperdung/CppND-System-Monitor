b871117d84c85197d5c71451c26ecc1042925635
vanperdung <dung.nv382001@gmail.com>
2024-11-03 23:43:14 +0700

    Clean code by following my format

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -12,59 +12,70 @@ using std::to_string;
 using std::vector;
 
 // DONE: An example of how to read data from the filesystem
-string LinuxParser::OperatingSystem() {
-  string line;
-  string key;
-  string value;
-  std::ifstream filestream(kOSPath);
-  if (filestream.is_open()) {
-    while (std::getline(filestream, line)) {
-      std::replace(line.begin(), line.end(), ' ', '_');
-      std::replace(line.begin(), line.end(), '=', ' ');
-      std::replace(line.begin(), line.end(), '"', ' ');
-      std::istringstream linestream(line);
-      while (linestream >> key >> value) {
-        if (key == "PRETTY_NAME") {
-          std::replace(value.begin(), value.end(), '_', ' ');
-          return value;
-        }
-      }
-    }
-  }
-  return value;
+string LinuxParser::OperatingSystem()
+{
+	string line;
+	string key;
+	string value;
+	std::ifstream filestream(kOSPath);
+	if (filestream.is_open())
+	{
+		while (std::getline(filestream, line))
+		{
+			std::replace(line.begin(), line.end(), ' ', '_');
+			std::replace(line.begin(), line.end(), '=', ' ');
+			std::replace(line.begin(), line.end(), '"', ' ');
+			std::istringstream linestream(line);
+			while (linestream >> key >> value)
+			{
+				if (key == "PRETTY_NAME")
+				{
+					std::replace(value.begin(), value.end(), '_', ' ');
+					return value;
+				}
+			}
+		}
+	}
+	return value;
 }
 
 // DONE: An example of how to read data from the filesystem
-string LinuxParser::Kernel() {
-  string os, kernel, version;
-  string line;
-  std::ifstream stream(kProcDirectory + kVersionFilename);
-  if (stream.is_open()) {
-    std::getline(stream, line);
-    std::istringstream linestream(line);
-    linestream >> os >> version >> kernel;
-  }
-  return kernel;
+string LinuxParser::Kernel()
+{
+	string os, kernel, version;
+	string line;
+	std::ifstream stream(kProcDirectory + kVersionFilename);
+	if (stream.is_open())
+	{
+		std::getline(stream, line);
+		std::istringstream linestream(line);
+		linestream >> os >> version >> kernel;
+	}
+	return kernel;
 }
 
 // BONUS: Update this to use std::filesystem
-vector<int> LinuxParser::Pids() {
-  vector<int> pids;
-  DIR* directory = opendir(kProcDirectory.c_str());
-  struct dirent* file;
-  while ((file = readdir(directory)) != nullptr) {
-    // Is this a directory?
-    if (file->d_type == DT_DIR) {
-      // Is every character of the name a digit?
-      string filename(file->d_name);
-      if (std::all_of(filename.begin(), filename.end(), isdigit)) {
-        int pid = stoi(filename);
-        pids.push_back(pid);
-      }
-    }
-  }
-  closedir(directory);
-  return pids;
+vector<int> LinuxParser::Pids()
+{
+	vector<int> pids;
+	DIR* directory = opendir(kProcDirectory.c_str());
+	struct dirent* file;
+	while ((file = readdir(directory)) != nullptr)
+	{
+		// Is this a directory?
+		if (file->d_type == DT_DIR)
+		{
+			// Is every character of the name a digit?
+			string filename(file->d_name);
+			if (std::all_of(filename.begin(), filename.end(), isdigit))
+			{
+				int pid = stoi(filename);
+				pids.push_back(pid);
+			}
+		}
+	}
+	closedir(directory);
+	return pids;
 }
 
 // TODO: Read and return the system memory utilization
